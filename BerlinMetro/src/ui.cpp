ea4fe9e38991e2eb3440d837fe975d4bf6c125e2
paulpatzer <ppatzer92@gmail.com>
2021-01-27 19:15:44 +0100

    Programm works completely now.

--- BerlinMetro/src/ui.cpp
+++ BerlinMetro/src/ui.cpp
@@ -67,28 +67,320 @@ void UI::outputAllConnections()
 	std::vector<Connection> connections = readConnections();
 
 	std::cout << "0. Sort by train line\n";
-	std::cout << "1. Sort by alphabet\n";
+	std::cout << "1. Sort by alphabet of departure station\n";
+	std::cout << "2. Sort by alphabet of destination station\n";
+	std::cout << "3. Sort by duration\n";
+	std::cout << "4. Sort by distance\n";
 
-	unsigned choice = readChoice("> Select optimization: ", 0, 1);
+	unsigned choice = readChoice("> Select optimization: ", 0, 4);
 	std::cout << "\n";
 
+	//sort by train line
 	if (choice == 0)
 	{
-	
-		//Sort by line
+		std::vector<Connection> connections = readConnections();
+		std::vector<Connection> connectionstemp2;
 
 		for (int i = 0; i < connections.size(); i++)
 		{
 
-			std::cout << connections[i].line << " " << connections[i].station_1 << " -> "
-				<< connections[i].station_2 << ": " << connections[i].first_and_second_number << " " << connections[i].third_number << std::endl;
+
+			Connection conntemp2;
+			conntemp2.line = connections[i].line;
+			conntemp2.station_1 = connections[i].station_1;
+			conntemp2.station_2 = connections[i].station_2;
+			conntemp2.third_number = connections[i].third_number;
+			conntemp2.first_and_second_number = connections[i].first_and_second_number;
+
+			connectionstemp2.push_back(conntemp2);
 
 		}
+
+
+		for (int i = 0; i < connectionstemp2.size(); i++)
+		{
+			std::string a = connectionstemp2[i].line;
+			std::string b = connectionstemp2[i].station_1;
+			std::string c = connectionstemp2[i].station_2;
+			int d = connectionstemp2[i].third_number;
+			double e = connectionstemp2[i].first_and_second_number;
+
+
+			int j = i - 1;
+
+			while (j >= 0 && a >= connectionstemp2[j].line)
+			{
+				connectionstemp2[j + 1].line = connectionstemp2[j].line;
+				connectionstemp2[j + 1].station_1 = connectionstemp2[j].station_1;
+				connectionstemp2[j + 1].station_2 = connectionstemp2[j].station_2;
+				connectionstemp2[j + 1].third_number = connectionstemp2[j].third_number;
+				connectionstemp2[j + 1].first_and_second_number = connectionstemp2[j].first_and_second_number;
+
+				j -= 1;
+			}
+
+			connectionstemp2[j + 1].line = a;
+			connectionstemp2[j + 1].station_1 = b;
+			connectionstemp2[j + 1].station_2 = c;
+			connectionstemp2[j + 1].third_number = d;
+			connectionstemp2[j + 1].first_and_second_number = e;
+		}
+
+		for (int i = 0; i < connectionstemp2.size(); i++)
+		{
+			std::cout << connectionstemp2[i].line << " " << connectionstemp2[i].station_1 << " -> "
+				<< connectionstemp2[i].station_2 << ": distance: " << connectionstemp2[i].first_and_second_number << " km, duration: " << connectionstemp2[i].third_number << " min" << std::endl;
+		}
+		
 		std::cout << "\n\n";
 	}
 	else if (choice == 1)
 	{
-		//Sort by alphabet
+		//Sort by alphabet for departure stations
+		std::vector<Connection> connectionstemp3;
+
+		for (int i = 0; i < connections.size(); i++)
+		{
+
+
+			Connection conntemp3;
+			conntemp3.line = connections[i].line;
+			conntemp3.station_1 = connections[i].station_1;
+			conntemp3.station_2 = connections[i].station_2;
+			conntemp3.third_number = connections[i].third_number;
+			conntemp3.first_and_second_number = connections[i].first_and_second_number;
+
+			connectionstemp3.push_back(conntemp3);
+
+		}
+
+
+		for (int i = 0; i < connectionstemp3.size(); i++)
+		{
+			std::string a = connectionstemp3[i].line;
+			std::string b = connectionstemp3[i].station_1;
+			std::string c = connectionstemp3[i].station_2;
+			int d = connectionstemp3[i].third_number;
+			double e = connectionstemp3[i].first_and_second_number;
+
+
+			int j = i - 1;
+
+			while (j >= 0 && b >= connectionstemp3[j].station_1)
+			{
+				connectionstemp3[j + 1].line = connectionstemp3[j].line;
+				connectionstemp3[j + 1].station_1 = connectionstemp3[j].station_1;
+				connectionstemp3[j + 1].station_2 = connectionstemp3[j].station_2;
+				connectionstemp3[j + 1].third_number = connectionstemp3[j].third_number;
+				connectionstemp3[j + 1].first_and_second_number = connectionstemp3[j].first_and_second_number;
+
+				j -= 1;
+			}
+
+			connectionstemp3[j + 1].line = a;
+			connectionstemp3[j + 1].station_1 = b;
+			connectionstemp3[j + 1].station_2 = c;
+			connectionstemp3[j + 1].third_number = d;
+			connectionstemp3[j + 1].first_and_second_number = e;
+		}
+
+		for (int i = 0; i < connectionstemp3.size(); i++)
+		{
+			std::cout << connectionstemp3[i].line << " " << connectionstemp3[i].station_1 << " -> "
+				<< connectionstemp3[i].station_2 << ": distance: " << connectionstemp3[i].first_and_second_number << "km, duration: " << connectionstemp3[i].third_number << " min" << std::endl;
+		}
+
+		std::cout << "\n\n";
+		
+
+	}
+
+
+	// Sort by alphabet for destination stations
+
+	else if (choice == 2)
+	{
+	std::vector<Connection> connectionstemp4;
+
+	for (int i = 0; i < connections.size(); i++)
+	{
+
+
+		Connection conntemp4;
+		conntemp4.line = connections[i].line;
+		conntemp4.station_1 = connections[i].station_1;
+		conntemp4.station_2 = connections[i].station_2;
+		conntemp4.third_number = connections[i].third_number;
+		conntemp4.first_and_second_number = connections[i].first_and_second_number;
+
+		connectionstemp4.push_back(conntemp4);
+
+	}
+
+
+	for (int i = 0; i < connectionstemp4.size(); i++)
+	{
+		std::string a = connectionstemp4[i].line;
+		std::string b = connectionstemp4[i].station_1;
+		std::string c = connectionstemp4[i].station_2;
+		int d = connectionstemp4[i].third_number;
+		double e = connectionstemp4[i].first_and_second_number;
+
+
+		int j = i - 1;
+
+		while (j >= 0 && c >= connectionstemp4[j].station_2)
+		{
+			connectionstemp4[j + 1].line = connectionstemp4[j].line;
+			connectionstemp4[j + 1].station_1 = connectionstemp4[j].station_1;
+			connectionstemp4[j + 1].station_2 = connectionstemp4[j].station_2;
+			connectionstemp4[j + 1].third_number = connectionstemp4[j].third_number;
+			connectionstemp4[j + 1].first_and_second_number = connectionstemp4[j].first_and_second_number;
+
+			j -= 1;
+		}
+
+		connectionstemp4[j + 1].line = a;
+		connectionstemp4[j + 1].station_1 = b;
+		connectionstemp4[j + 1].station_2 = c;
+		connectionstemp4[j + 1].third_number = d;
+		connectionstemp4[j + 1].first_and_second_number = e;
+	}
+
+	for (int i = 0; i < connectionstemp4.size(); i++)
+	{
+		std::cout << connectionstemp4[i].line << " " << connectionstemp4[i].station_1 << " -> "
+			<< connectionstemp4[i].station_2 << ": distance: " << connectionstemp4[i].first_and_second_number << " km, duration: " << connectionstemp4[i].third_number << " min" << std::endl;
+	}
+
+	std::cout << "\n\n";
+
+
+	}
+
+
+	// Sort by duration
+	
+	else if (choice == 3)
+	{
+	std::vector<Connection> connectionstemp5;
+
+	for (int i = 0; i < connections.size(); i++)
+	{
+
+
+		Connection conntemp5;
+		conntemp5.line = connections[i].line;
+		conntemp5.station_1 = connections[i].station_1;
+		conntemp5.station_2 = connections[i].station_2;
+		conntemp5.third_number = connections[i].third_number;
+		conntemp5.first_and_second_number = connections[i].first_and_second_number;
+
+		connectionstemp5.push_back(conntemp5);
+
+	}
+
+
+	for (int i = 0; i < connectionstemp5.size(); i++)
+	{
+		std::string a = connectionstemp5[i].line;
+		std::string b = connectionstemp5[i].station_1;
+		std::string c = connectionstemp5[i].station_2;
+		double d = connectionstemp5[i].third_number;
+		double e = connectionstemp5[i].first_and_second_number;
+
+
+		int j = i - 1;
+
+		while (j >= 0 && d >= connectionstemp5[j].third_number)
+		{
+			connectionstemp5[j + 1].line = connectionstemp5[j].line;
+			connectionstemp5[j + 1].station_1 = connectionstemp5[j].station_1;
+			connectionstemp5[j + 1].station_2 = connectionstemp5[j].station_2;
+			connectionstemp5[j + 1].third_number = connectionstemp5[j].third_number;
+			connectionstemp5[j + 1].first_and_second_number = connectionstemp5[j].first_and_second_number;
+
+			j -= 1;
+		}
+
+		connectionstemp5[j + 1].line = a;
+		connectionstemp5[j + 1].station_1 = b;
+		connectionstemp5[j + 1].station_2 = c;
+		connectionstemp5[j + 1].third_number = d;
+		connectionstemp5[j + 1].first_and_second_number = e;
+	}
+
+	for (int i = 0; i < connectionstemp5.size(); i++)
+	{
+		std::cout << connectionstemp5[i].line << " " << connectionstemp5[i].station_1 << " -> "
+			<< connectionstemp5[i].station_2 << ": distance: " << connectionstemp5[i].first_and_second_number << " km, duration: " << connectionstemp5[i].third_number << "min" << std::endl;
+	}
+
+	std::cout << "\n\n";
+
+
+	}
+
+	// Sort by distance
+
+	else if (choice == 4)
+	{
+	std::vector<Connection> connectionstemp6;
+
+	for (int i = 0; i < connections.size(); i++)
+	{
+
+
+		Connection conntemp6;
+		conntemp6.line = connections[i].line;
+		conntemp6.station_1 = connections[i].station_1;
+		conntemp6.station_2 = connections[i].station_2;
+		conntemp6.third_number = connections[i].third_number;
+		conntemp6.first_and_second_number = connections[i].first_and_second_number;
+
+		connectionstemp6.push_back(conntemp6);
+
+	}
+
+
+	for (int i = 0; i < connectionstemp6.size(); i++)
+	{
+		std::string a = connectionstemp6[i].line;
+		std::string b = connectionstemp6[i].station_1;
+		std::string c = connectionstemp6[i].station_2;
+		double d = connectionstemp6[i].third_number;
+		double e = connectionstemp6[i].first_and_second_number;
+
+
+		int j = i - 1;
+
+		while (j >= 0 && e >= connectionstemp6[j].first_and_second_number)
+		{
+			connectionstemp6[j + 1].line = connectionstemp6[j].line;
+			connectionstemp6[j + 1].station_1 = connectionstemp6[j].station_1;
+			connectionstemp6[j + 1].station_2 = connectionstemp6[j].station_2;
+			connectionstemp6[j + 1].third_number = connectionstemp6[j].third_number;
+			connectionstemp6[j + 1].first_and_second_number = connectionstemp6[j].first_and_second_number;
+
+			j -= 1;
+		}
+
+		connectionstemp6[j + 1].line = a;
+		connectionstemp6[j + 1].station_1 = b;
+		connectionstemp6[j + 1].station_2 = c;
+		connectionstemp6[j + 1].third_number = d;
+		connectionstemp6[j + 1].first_and_second_number = e;
+	}
+
+	for (int i = 0; i < connectionstemp6.size(); i++)
+	{
+		std::cout << connectionstemp6[i].line << " " << connectionstemp6[i].station_1 << " -> "
+			<< connectionstemp6[i].station_2 << ": distance: " << connectionstemp6[i].first_and_second_number << " km, duration: " << connectionstemp6[i].third_number << "min" << std::endl;
+	}
+
+	std::cout << "\n\n";
+
+
 	}
 
 }
